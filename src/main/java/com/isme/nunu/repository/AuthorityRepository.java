05a0229a417131047cfab43958bcee90e059570b
manhpt <llovelylip@gmail.com>
2021-01-10 16:15:43 +0700

    Initial version of mysqlreactjs generated by JHipster-6.10.1

---
+++ src/main/java/com/isme/nunu/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.isme.nunu.repository;
+
+import com.isme.nunu.domain.Authority;
+
+import org.springframework.data.r2dbc.repository.R2dbcRepository;
+
+/**
+ * Spring Data R2DBC repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends R2dbcRepository<Authority, String> {
+}
